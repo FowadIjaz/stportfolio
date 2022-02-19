ceb8c2a98e262eee304b6c77340680df6b2058fe
FowadIjaz <63612037+FowadIjaz@users.noreply.github.com>
2022-02-18 23:27:58 -0500

    modified text and image to fowad's stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,18 +7,18 @@ with open("style.css") as f:
 #####################
 # Header 
 st.write('''
-# Chanin Nantasenamat, Ph.D.
+# Fowad Ijaz, MBA
 ##### *Resume* 
 ''')
 
 image = Image.open('dp.png')
-st.image(image, width=150)
+st.image(image, width=200)
 
 st.markdown('## Summary', unsafe_allow_html=True)
 st.info('''
-- Experienced Educator, Researcher and Administrator with almost twenty years of experience in data-oriented environment and a passion for delivering insights based on predictive modeling. 
-- Strong verbal and written communication skills as demonstrated by extensive participation as invited speaker at `10` conferences as well as publishing 149 research articles.
-- Strong track record in scholarly research with H-index of `32` and total citation of 3200+.
+- Experienced business leader, decision-maker and administrator with 4 years of experience in data-oriented environment and a passion for delivering insights based on predictive modeling. 
+- Strong verbal and written communication skills as demonstrated by extensive participation in societies and associations.
+- Strong track record in business analysis and developing recommendations.
 ''')
 
 #####################
@@ -28,7 +28,7 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
-  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Chanin Nantasenamat</a>
+  <a class="navbar-brand" href="https://www.linkedin.com/in/fowad-ijaz/" target="_blank">Fowad Ijaz</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -42,9 +42,6 @@ st.markdown("""
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#work-experience">Work Experience</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#bioinformatics-tools">Bioinformatics Tools</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#social-media">Social Media</a>
@@ -91,20 +88,20 @@ st.markdown('''
 ## Education
 ''')
 
-txt('**Doctor of Philosophy** (Medical Technology), *Mahidol University*, Thailand',
-'2002-2006')
+txt('**Master Of Business Administration** (Business Analytics), *DeGroote School of Business, McMaster University*, Hamilton',
+'2018-2020')
 st.markdown('''
 - GPA: `3.89`
-- Research thesis entitled `Computer-aided molecular design for biological and chemical applications : Quantum chemical and machine learning approach`.
-- Received Royal Golden Jubilee Ph.D. Scholarship of `2.152 million THB` covering tuition and stipend.
-- Thesis awarded `1st` Prize by the National Research Council of Thailand.
+- Dean's List.
+- President, MBA Operations Management Association.
 ''')
 
-txt('**Bachelors of Science** (Biological Science), *Mahidol University International College*, Thailand',
-'1998-2002')
+txt('**Bachelors of Science** (Biological Science), *University of Toronto*, Toronto',
+'2013-2017')
 st.markdown('''
 - GPA: `3.65`
-- Graduated with First Class Honors.
+- Dean's List with First Class Honors.
+- Orientation leader, 2017.
 ''')
 
 #####################
@@ -112,105 +109,52 @@ st.markdown('''
 ## Work Experience
 ''')
 
-txt('**Head, Center of Data Mining and Biomedical Informatics**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2011-2021')
+txt('**Senior Business Analyst**, Health Benefits Management, TELUS Health, Toronto',
+'2021-Present')
 st.markdown('''
-- Managing a Center of `10` professors, researchers and students to ensure KPIs are strategically achieved namely to publish at least `20+` research publications annually. 
-- Actively took part in the talent hiring process as well as help employees to plan and develop their career path.
-- Set budget and handle procurement in order to facilitate education and research activities. Secured `> 10 million THB` budget.
-- Set and reflect on OKR on an annual basis to ensure productivity strategically matches the organization's direction.
+- Reporting to VP, Health Benefits Management, manage the business unit scorecard which secures compensation for 119 team members.
+- Developed internal communications for vice president to communicate new mandate of four key priorities.
+- Organized 2 training sessions to communicate the benefits of Google Suite to team members; 67 attendees.  
 ''')
 
-txt('**Associate Professor**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2012-2021')
-txt('**Assistant Professor**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2009-2012')
-txt('**Lecturer**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2006-2009')
+txt('**Operations Analyst, Co-op**, Health Benefits Management, TELUS Health, Toronto',
+'2020')
 st.markdown('''
-- Provided mentorship and supervision to junior faculty, researchers, Ph.D./M.Sc./B.Sc. students. Mentored `3` Post-doctoral fellows, supervised `13` Ph.D. students, supervised `8` M.Sc. students, supervised `13` B.Sc. students and hosted `6` visiting students from U.S., Sweden and India.
-- Wrote and applied for research grants. Served as Principal Investigator for research grants that have been awarded `12.5 million THB` and `1.117 million SEK` in research funding from Thai and Swedish grant agencies.
-- Conducted research by applying machine learning to computational drug discovery and ensuring that research output exceeds `20+` articles per year.
-- Taught `10+` undergraduate/graduate classes on Bioinformatics, Data Mining, Scientific Research and Presentation, Research Methodology, Graduate Seminar, Programming for Health Data Science, etc.
-- Peer reviewed `100+` research articles for leading scientific journals.
+-	Applied `analytical` and `problem-solving` skills to large datasets of healthcare provider data; used pivot tables, cost-benefit, and time-series analysis to create a business model that identified over `$100,000` in cost savings through the implementation of Robotic Process Automation.
+-	Reporting to the Senior Strategy Manager, undertook the implementation of TELUS Health’s first Robotic Process automation project, including drafting `business requirements` for the development team, creating a `risk management plan`, and `internal communications` to keep stakeholders appraised about project status. 
+-	Submitted monthly financial reports and quarterly scorecards to Vice President of Health Benefits Management team; automated the reporting process using VBA and pivot tables to reduce manual effort when pulling data from multiple sources.
+-	Completed ad hoc projects, including defining KPIs in collaboration with external consultants, responding to an RFP on short notice by gathering and distilling key capabilities of TELUS Health products and services, and creating a virtual care matrix to keep senior leadership appraised of the competitive landscape. 
 ''')
 
-txt('**Co-Chair**, International Conference on Pharmaceutical Bioinformatics at Pattaya, Thailand',
-'2016')
+txt('**Risk Analyst - Co-op**, Royal Bank of Canada, Toronto',
+'2019')
 st.markdown('''
-- Oversee all aspects of the conference preparations from conception to launch. This include inviting keynote and plenary speakers, create advertisement flyers, create abstract book, etc.
-- Conference attracted `200+` participants from `40+` countries from university and industry sector.
-- Chaired keynote session, technical workshop and some of the parallel sessions.
+- Used analytical skills to conduct qualitative and quantitative analysis to arrive at an internal risk rating for RBC clients, determining lending rates for non-banking financial institutions with assets ranging from $100M to $10B.
+- Authored `17` analyst reports and 250 counterparty risk reviews which summarized the analysis conducted and serve as justification behind RBC credit risk ratings, which were used to establish borrowing terms for RBC clients. Exceeded target of less than `10 percent` reviews outstanding.
 ''')
 
-txt('**Content Creator**, [Data Professor YouTube Channel](https://youtube.com/dataprofessor/)',
-'2019-Present')
+txt('**Policy Advisor - Co-op**, Government of Ontario, Toronto',
+'2019')
 st.markdown('''
-- `100,000+` subscribers on YouTube
-- Created `261` educational videos on data science, machine learning and bioinformatics as well as hosted several podcast episodes with data scientists.
-- Created `3` sponsored videos for Notion, Gradio and Classpert.
+-	Created project plan for delivery of Ontario Automotive Modernization Plan, including creating process flow for old vs new program delivery, engaging external stakeholders to get input on possible improvements, creating Gantt chart to establish team member roles & responsibilities, and risk mitigation plan.
+-	Analyzed SME proposals for grant funding based on financial feasibility, delivery team expertise, change management plan, and overall strength of proposal.
 ''')
 
-txt('**Content Creator**, [Coding Professor YouTube Channel](https://youtube.com/codingprofessor/)',
-'2019-Present')
-st.markdown('''
-- `3,200+` subscribers on YouTube
-- Created `38` educational videos on Python and R programming.
-''')
-
-txt('**Technical Writer**, [Data Professor Blog](https://data-professor.medium.com/) on Medium.com',
-'2019-Present')
-st.markdown('''
-- `4,100+` subscribers on Medium
-- Written `68` technical blogs on data science, machine learning and bioinformatics.
-''')
-
-#####################
-st.markdown('''
-## Bioinformatics Tools
-''')
-txt4('ABCpred', 'A web server for the discovery of acetyl- and butyryl-cholinesterase inhibitors', 'http://codes.bio/abcpred/')
-txt4('AutoWeka', 'An automated data mining software based on Weka', 'http://www.mt.mahidol.ac.th/autoweka/')
-txt4('ACPred', 'A computational tool for the prediction and analysis of anticancer peptides','http://codes.bio/acpred/')
-txt4('BioCurator', 'A web server for curating ChEMBL bioactivity data', 'http://codes.bio/biocurator/')
-txt4('CryoProtect', 'A web server for classifying antifreeze proteins from non-antifreeze proteins','http://codes.bio/cryoprotect/')
-txt4('ERpred', 'A web server for the prediction of subtype-specific estrogen receptor antagonists', 'http://codes.bio/erpred')
-txt4('HCVpred', 'A web server for predicting the bioactivity of Hepatitis C virus NS5B inhibitors', 'http://codes.bio/hemopred/')
-txt4('HemoPred', 'A web server for predicting the hemolytic activity of peptides', 'http://codes.bio/hemopred/')
-txt4('iQSP', 'A sequence-based tool for the prediction and analysis of quorum sensing peptides', 'http://codes.bio/iqsp/')
-txt4('Meta-iAVP', 'A sequence-based meta-predictor for improving the prediction of antiviral peptides', 'http://codes.bio/meta-iavp/')
-txt4('osFP', 'A web server for predicting the oligomeric state of fluorescent proteins', 'http://codes.bio/osfp/')
-txt4('PAAP', 'A web server for predicting antihypertensive activity of peptides', 'http://codes.bio/paap/')
-txt4('PepBio', 'A web server for predicting the bioactivity of host defense peptide', 'http://codes.bio/pepbio')
-txt4('PyBact', 'Open source software written in Python for bacterial identification', 'https://sourceforge.net/projects/pybact/')
-txt4('TargetAntiAngio', 'A sequence-based tool for the prediction and analysis of anti-angiogenic peptides','http://codes.bio/targetantiangio/')
-txt4('ThalPred', 'Development of decision model for discriminating Thalassemia trait and Iron deficiency anemia','http://codes.bio/thalpred/')
-txt4('THPep', 'A web server for predicting tumor homing peptides','http://codes.bio/thpep/')
 
 
 #####################
 st.markdown('''
 ## Skills
 ''')
-txt3('Programming', '`Python`, `R`, `Linux`')
+txt3('Programming', '`Python`, `R`')
 txt3('Data processing/wrangling', '`SQL`, `pandas`, `numpy`')
-txt3('Data visualization', '`matplotlib`, `seaborn`, `plotly`, `altair`, `ggplot2`')
-txt3('Machine Learning', '`scikit-learn`')
-txt3('Deep Learning', '`TensorFlow`')
-txt3('Web development', '`Flask`, `HTML`, `CSS`')
-txt3('Model deployment', '`streamlit`, `gradio`, `R Shiny`, `Heroku`, `AWS`, `Digital Ocean`')
+txt3('Data visualization', '`tableau`, `PowerBI`, `matplotlib`, `seaborn`, `plotly`, `ggplot2`')
+txt3('Web development', '`Django`, `HTML`, `CSS`')
+txt3('Model deployment', '`streamlit`, `Heroku`, `AWS`')
 
 #####################
 st.markdown('''
 ## Social Media
 ''')
-txt2('LinkedIn', 'https://www.linkedin.com/in/chanin-nantasenamat')
-txt2('Twitter', 'https://twitter.com/thedataprof')
-txt2('GitHub', 'https://github.com/chaninn/')
-txt2('', 'https://github.com/chaninlab/')
-txt2('', 'https://github.com/dataprofessor')
-txt2('ORCID', 'http://orcid.org/0000-0003-1040-663X')
-txt2('Scopus', 'http://www.scopus.com/authid/detail.url?authorId=12039071300')
-txt2('ResearcherID', 'http://www.researcherid.com/rid/F-1021-2010')
-txt2('ResearchGate', 'https://www.researchgate.net/profile/Chanin_Nantasenamat')
-txt2('Publons', 'https://publons.com/a/303133/')
+txt2('LinkedIn', 'https://www.linkedin.com/in/fowad-ijaz/')
+txt2('GitHub', 'https://github.com/FowadIjaz')
